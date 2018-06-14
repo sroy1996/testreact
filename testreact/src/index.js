0da675386dee60b6dc82b7f526323d0f66685a0f
sroy1996 <n6365206@gmail.com>
2018-06-14 16:08:56 +0800

    0614

--- testreact/src/index.js
+++ testreact/src/index.js
@@ -1,41 +1,71 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-class ShopItem extends React.Component{
-	render(){
-		return(
+
+class ShopItem extends React.Component {
+
+	clickDel(){  //對照一下剛剛在 constructor 中 bind 的形式
+		this.props.delItem( this.props.index );
+	}
+	
+	render() {
+		return (
 			<tr>
 				<td>{this.props.value}</td>
-				<td>g</td>
-				</tr>
+				<td><button onClick={this.clickDel.bind(this)}>刪除</button></td>
+			</tr>
 		)
 	}
 }
 
-class App extends React.Component{
-	constructor(props){
+
+class App extends React.Component {
+	constructor(props) {
 		super(props);
-		this.state ={
-			items:['ni','hao','r']
+		this.state = {
+			items: ['234','aaaaa','cdefg','Zz'],  //呈現用的清單
+			sample: '原來的'
 		};
 		this.testClick = this.testClick.bind(this);
+		this.changeText = this.changeText.bind(this);
 	}
-	testClick(){
+	
+	changeText( event ) {
+		this.setState( {sample: event.target.value}  );
+	}
+	
+	testClick() {
 		const data = this.state.items;
-		data.push('ooxx');
-		
+		data.push( this.state.sample );
+		this.setState( {
+			items: data,
+			sample: ''
+		} );
 	}
-	render(){
-		return(
-		<div>
-			<table>
-				<tbody>
-				{this.state.items.map( item => ( <ShopItem value ={item} />))}					
-				</tbody>
-			</table>
-			<button onClick={this.testClick}>按鈕</button>
-		</div>
+	
+	delItem(index){
+		const data = this.state.items;
+		data.splice(index, 1);
+		this.setState( { items: data } );
+	}
+	
+	render() {
+		return (
+			<div>
+				<input type="text" value={this.state.sample} 
+					onChange={this.changeText} />
+				<button onClick={this.testClick}>按鈕</button>
+				
+				<table>
+					<tbody>
+						{this.state.items.map( (aa,idx) => (
+							<ShopItem value={aa} index={idx} 
+								delItem={this.delItem.bind(this)} />
+						))}
+					</tbody>
+				</table>
+			</div>
 		)
 	}
 }
 
-ReactDom.render(<App />,document.getElementById('root'));
+ReactDom.render( <App />, document.getElementById('root') );
